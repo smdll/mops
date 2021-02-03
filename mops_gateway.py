9329365459ce623d2dd0670aa339458a8a0c1a6d
smd <songmingda1234@hotmail.com>
2021-02-03 15:36:24 +0800

    2021.2.3

--- mops_gateway.py
+++ mops_gateway.py
@@ -4,12 +4,24 @@ from apscheduler import BackgroundScheduler
 import pytz
 import math
 import time
+from zeroconf import ServiceBrowser, Zeroconf
 
 timezone = pytz.timezone("Asia/Shanghai")
 app = Flask(__name__)
 scheduler = BackgroundScheduler(timezone=timezone)
 sunrise_time = {"hour": 7, "minute": 0}
 sunset_time = {"hour": 18, "minute": 0}
+zeroconf = Zeroconf()
+listener = mDNSListener()
+browser = ServiceBrowser(zeroconf, "_http._tcp.local.", listener)
+
+class mDNSListener:
+    def remove_service(self, zeroconf, type, name):
+        print("Service %s removed" % (name,))
+
+    def add_service(self, zeroconf, type, name):
+        info = zeroconf.get_service_info(type, name)
+        print("Service %s added, service info: %s" % (name, info))
 
 #TODO: have a list of mops switches to iterate
 def sunrise_task():
